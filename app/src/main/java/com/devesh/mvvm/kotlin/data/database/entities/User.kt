e8f241a3cf6eab17d7297a61b91ef2a770fbb67b
DeveshLoktra <devesh@loktra.com>
2019-07-14 01:23:44 +0530

    Removed Unused code from User data class

--- app/src/main/java/com/devesh/mvvm/kotlin/data/database/entities/User.kt
+++ app/src/main/java/com/devesh/mvvm/kotlin/data/database/entities/User.kt
@@ -3,7 +3,6 @@ package com.devesh.mvvm.kotlin.data.database.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-/*
 const val CURRENT_USER_ID = 0
 
 @Entity
@@ -16,22 +15,6 @@ data class User(
     var created_at: String? = null,
     var updated_at: String? = null
 ) {
-    @PrimaryKey(autoGenerate = false)
-    val  uid: Int = CURRENT_USER_ID
-}*/
-
-const val CURRENT_USER_ID = 0
-
-@Entity
-data class User(
-    var id: Int? = null,
-    var name: String? = null,
-    var email: String? = null,
-    var password: String? = null,
-    var email_verified_at: String? = null,
-    var created_at: String? = null,
-    var updated_at: String? = null
-){
     @PrimaryKey(autoGenerate = false)
     var uid: Int = CURRENT_USER_ID
 }
